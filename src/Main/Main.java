3b5b8118c30b91c8396f8b0b5cdf7cf5c6352411
Ryu CAPISTRANO <e185753@ie.u-ryukyu.ac.jp>
2019-01-15 09:45:17 +0900

    prog2 ex4 step1 READM pull-push-test

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,11 @@
 import java.util.Date;
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
+
     public class MazeData {
-        public int 7;  // 迷路の横の大きさ
-        public int 8;  // 迷路の縦の大きさ
+        public int width = 7;  // 迷路の横の大きさ
+        public int height = 8;  // 迷路の縦の大きさ
+    }
 }
